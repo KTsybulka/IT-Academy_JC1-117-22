13f83af76954bfdc26ad17b2430fb668b27290eb
Kirill Tsybulka <ktsybulka11@gmail.com>
2022-09-12 00:02:58 +0300

    feat: add homework lesson 1

---
+++ Lection1_Task4.java
@@ -0,0 +1,7 @@
+public class Lection1_Task4 {
+    public static void main(String ...args){
+        System.out.println("Java: version 11.");
+        System.out.println("JVM: HotSpot.");
+
+    }
+}
