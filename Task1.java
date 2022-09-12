1452e91c4d7982ced562f7f32236ae74c8d426e9
Kirill Tsybulka <ktsybulka11@gmail.com>
2022-09-12 21:43:15 +0300

    Delete Task1.java

--- Task1.java
+++
@@ -1,9 +0,0 @@
-public class Task1 {
-    public static void main (String ...args){
-        int x = 15;
-        int y = 6;
-        System.out.println( x / y);
-        System.out.println( x % y);
-    }
-
-}
