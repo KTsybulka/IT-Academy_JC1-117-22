0ea0dd5c6bfac3c488be84851237ce758d611850
Kirill Tsybulka <ktsybulka11@gmail.com>
2022-09-12 21:42:01 +0300

    feet: added solutions to Lesson 3 tasks

---
+++ Lesson03Task02.java
@@ -0,0 +1,20 @@
+public class Lesson03Task02 {
+    public static void main(String ...adsg){
+        byte dayOfWeek = 6;
+        if (dayOfWeek == 1){
+            System.out.println("It's Monday.");
+        } else if (dayOfWeek == 2){
+            System.out.println("It's Tuesday.");
+        } else if (dayOfWeek == 3){
+            System.out.println("It's Wensday.");
+        } else if (dayOfWeek == 4){
+            System.out.println("It's Thursday.");
+        } else if (dayOfWeek == 5){
+            System.out.println("It's Friday.");
+        } else if (dayOfWeek == 6){
+            System.out.println("It's Saterday.");
+        } else {
+            System.out.println("It's Sunday.");
+        }
+    }
+}
---
+++ Lesson03Task03.java
@@ -0,0 +1,12 @@
+public class Lesson03Task03 {
+    public static void main(String ...args){
+        byte curentTime = 11;
+        if (curentTime >= 17){
+            System.out.println("Good evening!");
+        } else if (curentTime > 11){
+            System.out.println("Good afternoon!");
+        } else {
+            System.out.println("Good morning!");
+        }
+    }
+}
