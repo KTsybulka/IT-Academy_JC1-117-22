43273423bb90ac51d5e48907d899470dd4975f8a
Kirill Tsybulka <ktsybulka11@gmail.com>
2022-09-18 23:53:45 +0300

    Delete Task3.java

--- Task3.java
+++
@@ -1,7 +0,0 @@
-public class Task3 {
-    public static void main (String ...args){
-        int radiusCircle = 15;
-        System.out.println("Circle's squer is " +  3.14 * radiusCircle * radiusCircle + " mm2.");
-        System.out.println("Circle's length is " +  2 * 3.14 * radiusCircle + " mm.");
-    }
-}
