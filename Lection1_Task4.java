e70943470066420bc397be97ed1373eaad006729
Kirill Tsybulka <ktsybulka11@gmail.com>
2022-09-18 23:51:01 +0300

    Delete Lection1_Task4.java

--- Lection1_Task4.java
+++
@@ -1,7 +0,0 @@
-public class Lection1_Task4 {
-    public static void main(String ...args){
-        System.out.println("Java: version 11.");
-        System.out.println("JVM: HotSpot.");
-
-    }
-}
